e41d381d2c756ba8e77938aeed3294e94e439d04
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-05-29 21:43:52 +0100

    Add button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+// import SaveIcon from '@material-ui/icon/Save';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button
+        onClick={()=> alert(2+2)}
+        variant="outlined"
+        color="primary"
+      >
+        Hello world
+      </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
